0e5a48492248ad7af0e43e77d46715c8aa8261e0
Antônio Felipe <felipe.marques@animati.com.br>
2025-05-20 07:00:15 -0300

    feat(student): improving student page html and css. Adding student card and filter components

--- src/app/features/student/pages/students-page/students-page.component.ts
+++ src/app/features/student/pages/students-page/students-page.component.ts
@@ -1,19 +1,43 @@
-import { Component, inject } from '@angular/core';
+import { Student } from '../../models/student.interface';
+import { AsyncPipe } from '@angular/common';
 import { StudentService } from '../../services/student.service';
+import { map, Observable } from 'rxjs';
+import { inject, Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { StudentCardComponent } from '../../components/student-card/student-card.component';
+import { StudentFilterComponent } from '../../../../shared/components/student-filter/student-filter.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-students-page',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    RouterLink,
+    ReactiveFormsModule,
+    StudentCardComponent,
+    StudentFilterComponent,
+  ],
   templateUrl: './students-page.component.html',
   styleUrl: './students-page.component.scss'
 })
 export class StudentsPageComponent {
   private readonly studentService = inject(StudentService);
 
+  private pageNumber = 0;
+  private pageSize = 15;
+  students$ = new Observable<Student[]>();
+
+  searchField = new FormControl('');
+
   constructor() {
-    this.studentService.getStudents(0, 15).subscribe({
-      next: () => console.log('Pronto'),
-      // error: () => console.log('Algo deu errado'),
-    })
+    this.getStudents();
+  }
+
+  private getStudents(): void {
+    this.students$ = this.studentService
+      .getStudents(this.pageNumber, this.pageSize)
+      .pipe(
+        map(response => response.content)
+      );
   }
 }
