f156ededf5b60ae5cb37a0441380f6d7cee9f7ec
Antônio Felipe <felipe.marques@animati.com.br>
2025-05-25 13:54:10 -0300

    refactor(student): organizing student-page.components imports

--- src/app/features/student/pages/students-page/students-page.component.ts
+++ src/app/features/student/pages/students-page/students-page.component.ts
@@ -2,13 +2,21 @@ import { Student } from '../../models/student.interface';
 import { AsyncPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { StudentService } from '../../services/student.service';
-import { finalize, map, Observable } from 'rxjs';
+import { SpinnerComponent } from '../../../../shared/components/spinner/spinner.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { StudentCardComponent } from '../../components/student-card/student-card.component';
 import { StudentFilterComponent } from '../../../../shared/components/student-filter/student-filter.component';
-import { inject, Component, Renderer2 } from '@angular/core';
 import { StudentCreationModalComponent } from '../../components/student-creation-modal/student-creation-modal.component';
-import { SpinnerComponent } from '../../../../shared/components/spinner/spinner.component';
+import { 
+  map, 
+  finalize, 
+  Observable,
+} from 'rxjs';
+import { 
+  inject, 
+  Component, 
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-students-page',
@@ -28,7 +36,7 @@ export class StudentsPageComponent {
   private readonly renderer = inject(Renderer2);
   private readonly studentService = inject(StudentService);
   private pageNumber = 0;
-  private pageSize = 30;
+  private pageSize = 40;
 
   protected students$ = new Observable<Student[]>();
   protected isLoading: boolean = false;
