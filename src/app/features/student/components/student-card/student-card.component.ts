0de517b33a7f47c1c1a3fae006db94e5dff493fe
Antônio Felipe <felipe.marques@animati.com.br>
2025-05-31 08:53:31 -0300

    feat(student): update active status

--- src/app/features/student/components/student-card/student-card.component.ts
+++ src/app/features/student/components/student-card/student-card.component.ts
@@ -1,6 +1,6 @@
 import { Student } from '../../models/student.interface';
 import { PhoneNumberPipe } from '../../../../shared/pipes/phone-number.pipe';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-student-card',
@@ -10,10 +10,28 @@ import { Component, Input } from '@angular/core';
 })
 export class StudentCardComponent {
   @Input({ required: true }) student!: Student;
+  @Output() private readonly activeStudentEmitter = new EventEmitter<string>(); 
+  @Output() private readonly inactiveStudentEmitter = new EventEmitter<string>(); 
 
   openActionsMenu: boolean = false;
 
   toogleMenu(): void {
     this.openActionsMenu = !this.openActionsMenu;
+  }
+
+  changeStudentActiveStatus(): void {
+    if (this.student.active) {
+      this.inactivateStudent();
+    } else {
+      this.activateStudent();
+    }
+  }
+
+  activateStudent(): void {
+    this.activeStudentEmitter.emit(this.student.id);
+  }
+
+  inactivateStudent(): void {
+    this.inactiveStudentEmitter.emit(this.student.id);
   }
 }
