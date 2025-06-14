90423631ffad362e5371cb72c6a252550c099551
Antônio Felipe <felipe.marques@animati.com.br>
2025-06-14 15:58:36 -0300

    fix(payment): fixing payment initial router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './features/home/pages/home-page/home-page.component';
 import { StudentsPageComponent } from './features/student/pages/students-page/students-page.component';
+import { PaymentCreationPageComponent } from './features/payment/pages/payment-creation-page/payment-creation-page.component';
 
 export const routes: Routes = [
     {
-        path: "home",
+        path: 'home',
         component: HomePageComponent,
     },
     {
-        path: "students",
+        path: 'students',
         component: StudentsPageComponent,
     },
     {
-        path: "**",
-        redirectTo: "/home",
+        path: 'payments',
+        component: PaymentCreationPageComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '/home',
     },
 ];
