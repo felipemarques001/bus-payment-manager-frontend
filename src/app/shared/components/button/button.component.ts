1314c8c6d3048a461aa8ee79c03a6b01fd8390a9
Felipe Marques <felipemarquesgg@outlook.com>
2025-07-02 20:35:16 -0300

    fix(project): fix the disabled button error

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
+  @Input() isDisabled: boolean = false;
   @Input() iconName: string | null = null;
   @Input({ required: true }) bodyText: string = '';
   @Input({ required: true }) type: 'primary-btn' | 'secondary-btn' | 'delete-btn' = 'secondary-btn';
