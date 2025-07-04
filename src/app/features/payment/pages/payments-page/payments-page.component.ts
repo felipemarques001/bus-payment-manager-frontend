2e7dd0d5618e820a30396a5e60c822ec0b6d7059
Felipe Marques <felipemarquesgg@outlook.com>
2025-07-03 21:27:13 -0300

    refactor(payment): change id nomenclature and organize imports in payments page component

--- src/app/features/payment/pages/payments-page/payments-page.component.ts
+++ src/app/features/payment/pages/payments-page/payments-page.component.ts
@@ -1,16 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+import { PageResponse } from '../../../../shared/models/page-response.interface';
+import { ToastrService } from 'ngx-toastr';
+import { PaymentSummary } from '../../models/payment-summary.interface';
+import { PaymentService } from '../../services/payment.service';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
 import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
-import { PaymentService } from '../../services/payment.service';
-import { catchError, finalize, map, Observable, of } from 'rxjs';
-import { PageResponse } from '../../../../shared/models/page-response.interface';
-import { PaymentSummary } from '../../models/payment-summary.interface';
-import { AsyncPipe } from '@angular/common';
-import { PaymentSummaryCardComponent } from '../../components/payment-summary-card/payment-summary-card.component';
-import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
-import { PaymentSummaryCardSkeletonComponent } from '../../components/payment-summary-card-skeleton/payment-summary-card-skeleton.component';
 import { PageCounterComponent } from '../../../../shared/components/page-counter/page-counter.component';
+import { Component, inject, OnInit } from '@angular/core';
+import { PaymentSummaryCardComponent } from '../../components/payment-summary-card/payment-summary-card.component';
+import { PaymentSummaryCardSkeletonComponent } from '../../components/payment-summary-card-skeleton/payment-summary-card-skeleton.component';
+import { catchError, finalize, map, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-payments-page',
